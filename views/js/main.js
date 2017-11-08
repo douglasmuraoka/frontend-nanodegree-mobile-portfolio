e6811f08b949f10be91736f35423da5186b3de3d
Douglas Muraoka <douglas.muraoka@gmail.com>
2017-11-08 00:51:34 -0200

    fix: Remove bottleneck on random pizzas creation
    Creates pizza DOM node from a template, instead of creating
    from zero every iteration.

--- views/js/main.js
+++ views/js/main.js
@@ -358,8 +358,8 @@ var makeRandomPizza = function() {
   return pizza;
 };
 
-// returns a DOM element for each pizza
-var pizzaElementGenerator = function(i) {
+// Creates a pizza template to be used when creating random pizzas DOM elements
+var pizzaElementTemplate = function(i) {
   var pizzaContainer,             // contains pizza title, image and list of ingredients
       pizzaImageContainer,        // contains the pizza image
       pizzaImage,                 // the pizza image itself
@@ -372,28 +372,41 @@ var pizzaElementGenerator = function(i) {
   pizzaImage = document.createElement("img");
   pizzaDescriptionContainer = document.createElement("div");
 
-  pizzaContainer.classList.add("randomPizzaContainer");
-  pizzaContainer.style.width = "33.33%";
-  pizzaContainer.style.height = "325px";
-  pizzaContainer.id = "pizza" + i;                // gives each pizza element a unique id
-  pizzaImageContainer.style.width="35%";
+  pizzaContainer.className = "randomPizzaContainer";
+  pizzaImageContainer.className = "randomPizzaImageContainer";
 
   pizzaImage.src = "../dist/views/img/pizza.webp";
-  pizzaImage.classList.add("img-responsive");
+  pizzaImage.className = "img-responsive";
   pizzaImageContainer.appendChild(pizzaImage);
   pizzaContainer.appendChild(pizzaImageContainer);
 
-
-  pizzaDescriptionContainer.style.width="65%";
+  pizzaDescriptionContainer.className = "randomPizzaDescriptionContainer";
 
   pizzaName = document.createElement("h4");
-  pizzaName.innerHTML = randomName();
   pizzaDescriptionContainer.appendChild(pizzaName);
 
   ul = document.createElement("ul");
-  ul.innerHTML = makeRandomPizza();
   pizzaDescriptionContainer.appendChild(ul);
   pizzaContainer.appendChild(pizzaDescriptionContainer);
+
+  return pizzaContainer;
+};
+
+// returns a DOM element for each pizza
+var pizzaElementGenerator = function(pizzaTemplate) {
+  var pizzaContainer,             // contains pizza title, image and list of ingredients
+      pizzaDescriptionContainer,  // contains the pizza title and list of ingredients
+      pizzaName,                  // the pizza name itself
+      ul;                         // the list of ingredients
+
+  pizzaContainer = pizzaTemplate.cloneNode(true);
+  pizzaDescriptionContainer = pizzaContainer.lastElementChild;
+
+  pizzaName = pizzaDescriptionContainer.firstElementChild;
+  pizzaName.innerHTML = randomName();
+
+  ul = pizzaDescriptionContainer.lastElementChild;
+  ul.innerHTML = makeRandomPizza();
 
   return pizzaContainer;
 };
@@ -401,9 +414,10 @@ var pizzaElementGenerator = function(i) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
+var pizzaTemplate = pizzaElementTemplate();
+var pizzasDiv = document.getElementById("randomPizzas");
 for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
-  pizzasDiv.appendChild(pizzaElementGenerator(i));
+  pizzasDiv.appendChild(pizzaElementGenerator(pizzaTemplate));
 }
 
 // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
