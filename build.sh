085494321c45f0c953f1b7e5e74abb76b00be437
Douglas Muraoka <douglas.muraoka@gmail.com>
2017-11-08 22:03:14 -0200

    docs: Update README

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+npm install grunt
+npm install grunt-responsive-images
+npm install grunt-cwebp
+npm install grunt-contrib-clean
+npm install grunt-mkdir
+npm install grunt-contrib-cssmin
+npm install grunt-contrib-uglify
+npm install grunt-contrib-htmlmin
+npm install -g grunt-cli
+grunt
